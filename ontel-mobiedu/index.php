667d80e8ea78b438eae04b3c349e563d53bb1c80
Tung <tungnt@vano.vn>
2021-07-16 13:41:07 +0700

    init

---
+++ ontel-mobiedu/index.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <title>MobiEdu | Ontel,JSC</title>
+  </head>
+  <body>
+  	<section id="header">
+  		<nav class="navbar navbar-expand-lg fixed-top navbar-light background-gradient-custom">
+			<div class="container">
+				<a class="navbar-brand" href="#">
+					<img src="assets/images/logo.png" alt="Logo">
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
+					<ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-white">
+					    <li class="nav-item me-3">
+					    	<a class="nav-link active text-white" aria-current="page" href="#">Trang chủ</a>
+					    </li>
+					    <li class="nav-item me-3">
+					    	<a class="nav-link text-white" href="#combo">Khóa học</a>
+					    </li>
+					    <li class="nav-item me-3">
+					    	<a class="nav-link text-white" href="#guide">Hướng dẫn</a>
+					    </li>
+					    <li class="nav-item me-3">
+					    	<a class="nav-link text-white btn custom-button" href="sms:9285&body=ESP">Mua EduPlus mSkill</a>
+					    </li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+  	</section>
+  	<section id="content">
+  		<div class="container-fluid background-gradient-custom">
+  			<div class="row padding-top-custom">
+  				<div class="col-sm-12 col-md-6 p-4">
+  					<div class="align-content-center">
+  						<h1 class="text-white">EduPlus mSkill</h1>
+	  					<h3 class="text-white">Tiếng Anh từ A-Z</h3>
+	  					<ul>
+	  						<li>500MB truy cập internet mỗi ngày.</li>
+	  						<li>10 phút gọi nội mạng mỗi ngày.</li>
+	  						<li>Miễn cước thuê bao gói combo khóa học Tiếng Anh từ A-Z.</li>
+	  						<li>Miễn cước data truy cập mskill.mobiedu.vn.</li>
+	  					</ul>
+	  					<a class="text-white btn custom-button" href="sms:9285&body=ESP">ĐĂNG KÝ NGAY</a>
+  					</div>
+  				</div>
+  				<div class="col-sm-12 col-md-6 p-4">
+  					<img src="assets/images/header-picture.png" alt="Picture header" class="img-fluid">
+  				</div>
+  			</div>
+  			<div>
+  				<a class="text-white btn button-gradient" href="#"><i class="fas fa-arrow-down"></i></a>
+  			</div>
+  		</div>
+  		<div class="container padding-top-custom" id="combo">
+  			<div class="row pb-5">
+  				<h3 class="text-center title-gradient">KHÓA HỌC</h3>
+  				<hr>
+  			</div>
+  			<div class="row justify-content-md-center">
+  				<div class="col text-center">
+					<div class="shadow border-shaddow">
+						<p>Khóa học combo</p>
+						<h1 style="color: rgba(242, 154, 48, 1);">Tiếng Anh từ A-Z</h3>
+						<img src="assets/images/pic1.png" class="img-fluid">
+						<div>
+							<p>Bao gồm <span><strong>5 khóa học Tiếng Anh</strong></span></p>
+							<p>Học không giới hạn nội dung trong gói</p>
+							<p>Miễn phí data khi đăng ký gói</p>
+						</div>
+						<a href="https://mskill.mobiedu.vn/goi-cuoc/tieng-anh-tu-a-den-z-ngay-2017" class="btn custom-button text-white">XEM THỬ</a>
+					</div>
+  				</div>
+  				<div class="col text-center">
+					<div class="shadow border-shaddow">
+						<p>Khóa học combo</p>
+						<h1 style="color: rgba(214, 137, 232, 1);">Hình họa cơ bản</h3>
+						<img src="assets/images/pic2.png" class="img-fluid">
+						<div>
+							<p>Bao gồm <span><strong>5 khóa học Hình họa</strong></span></p>
+							<p>Học không giới hạn nội dung trong gói</p>
+							<p>Miễn phí data khi đăng ký gói</p>
+						</div>
+						<a href="https://mskill.mobiedu.vn/goi-cuoc/hinh-hoa-co-ban-ngay-2021" class="btn custom-button text-white">XEM THỬ</a>
+					</div>
+  				</div>
+  			</div>
+  		</div>
+  		<div class="container padding-top-custom" id="guide">
+  			<div class="row pb-5">
+  				<h3 class="text-center title-gradient">HƯỚNG DẪN ĐĂNG KÝ</h3>
+  				<hr>
+  				<p class="text-center">Soạn tin <strong>DK &#60;cú pháp&#62; </strong>gửi <strong>9285</strong></p>
+  			</div>
+  			<div class="row justify-content-md-center">
+  				<div class="col text-center">
+					<div class="shadow border-shaddow p-3" style="background-color: #FFF6EB; border-radius: 2rem;">
+						<p>Gói ngày</p>
+						<h3>Tiếng Anh từ A-Z</h3>
+						<hr>
+						<h4 style="color: #F29A30;">5.000đ/ngày</h4>
+						<a class="btn btn-warning text-white" style="border-radius: 2rem;" href="sms:9285&body=DK EN1">Đăng ký ngay</a>
+						<hr>
+						<p>Cú pháp đăng ký</p>
+						<h4>DK EN1 gửi 9285</h4>
+						<hr>
+						<p>Cú pháp hủy</p>
+						<h4>HUY EN1 gửi 9285</h4>
+						<hr>
+						<p>Tra cứu gói cước</p>
+						<h4>KT gửi 9285</h4>
+					</div>
+  				</div>
+  				<div class="col text-center">
+					<div class="shadow border-shaddow p-3" style="background-color: #FFEBD3; border-radius: 2rem;">
+						<p>Gói tuần</p>
+						<h3>Tiếng Anh từ A-Z</h3>
+						<hr>
+						<h4 style="color: #F29A30;">20.000đ/7 ngày</h4>
+						<a class="btn btn-warning text-white" style="border-radius: 2rem;" href="sms:9285&body=DK EN7">Đăng ký ngay</a>
+						<hr>
+						<p>Cú pháp đăng ký</p>
+						<h4>DK EN7 gửi 9285</h4>
+						<hr>
+						<p>Cú pháp hủy</p>
+						<h4>HUY EN7 gửi 9285</h4>
+						<hr>
+						<p>Tra cứu gói cước</p>
+						<h4>KT gửi 9285</h4>
+					</div>
+  				</div>
+  				<div class="col text-center">
+					<div class="shadow border-shaddow p-3" style="background-color: #FCF1FF; border-radius: 2rem;">
+						<p>Gói ngày</p>
+						<h3>Hình họa cơ bản</h3>
+						<hr>
+						<h4 style="color: #D689E8;">5.000đ/ngày</h4>
+						<a class="btn btn-warning text-white" style="border-radius: 2rem;" href="sms:9285&body=DK HH1">Đăng ký ngay</a>
+						<hr>
+						<p>Cú pháp đăng ký</p>
+						<h4>DK HH1 gửi 9285</h4>
+						<hr>
+						<p>Cú pháp hủy</p>
+						<h4>HUY HH1 gửi 9285</h4>
+						<hr>
+						<p>Tra cứu gói cước</p>
+						<h4>KT gửi 9285</h4>
+					</div>
+  				</div>
+  				<div class="col text-center">
+					<div class="shadow border-shaddow p-3" style="background-color: #F8DCFF; border-radius: 2rem;">
+						<p>Gói tuần</p>
+						<h3>Hình họa cơ bản</h3>
+						<hr>
+						<h4 style="color: #D689E8;">20.000đ/7 ngày</h4>
+						<a class="btn btn-warning text-white" style="border-radius: 2rem;" href="sms:9285&body=DK HH7">Đăng ký ngay</a>
+						<hr>
+						<p>Cú pháp đăng ký</p>
+						<h4>DK HH7 gửi 9285</h4>
+						<hr>
+						<p>Cú pháp hủy</p>
+						<h4>HUY HH7 gửi 9285</h4>
+						<hr>
+						<p>Tra cứu gói cước</p>
+						<h4>KT gửi 9285</h4>
+					</div>
+  				</div>
+  			</div>
+  		</div>
+  		<div class="container-fluid" style="background-color: rgba(212, 213, 243, 0.5);" id="eduplus">
+  			<div class="container">
+  				<div class="row justify-content-md-center padding-top-custom" style="padding-bottom: 16em;">
+	  				<div class="col">
+	  					<div class="card-bg">
+	  						<div class="content-center">
+	  							<h1>EduPlus mSkill</h1>
+		  						<h2>Tiếng anh từ A-Z</h2>
+		  						<ul>
+			  						<li>500MB truy cập internet mỗi ngày.</li>
+			  						<li>10 phút gọi nội mạng mỗi ngày.</li>
+			  						<li>Miễn cước thuê bao gói combo khóa học Tiếng Anh từ A-Z.</li>
+			  						<li>Miễn cước data truy cập mskill.mobiedu.vn.</li>
+			  					</ul>
+	  						</div>
+	  						<img src="assets/images/3.png" class="img-fluid" style="position: absolute;">
+	  					</div>
+	  				</div>
+	  				<div class="col col-md-auto">
+	  					<div class="text-center card-bg">
+  							<h3>6.000đ/ngày</h3>
+	  						<a href="sms:9285&body=ESP" class="btn custom-button text-white">ĐĂNG KÝ NGAY</a>
+	  						<hr>
+	  						<p>Cú pháp đăng ký</p>
+	  						<h4>DK EPA hoặc EPA gửi 999</h4>
+	  						<hr>
+	  						<p>Cú pháp hủy</p>
+	  						<h4>HUY EPA gửi 999</h4>
+	  						<hr>
+	  						<p>Tra cứu gói cước</p>
+	  						<h4>KT gửi 9285</h4>	
+	  					</div>
+	  				</div>
+	  			</div>
+  			</div>
+  		</div>
+  	</section>
+  	<section id="footer">
+  		<div class="container-fluid p-4" style="background-color: #B8C7E1;">
+  			<div class="container">
+  				<div class="row">
+  				<div class="col-sm-12 col-md-4">
+  					<img src="assets/images/logo-ontel.png" class="img-fluid">
+  				</div>
+  				<div class="col-sm-12 col-md-8">
+  					<ul>
+  						<li><strong>Chủ sở hữu Website: </strong>Công ty cổ phần ONTEL</li>
+  						<li><strong>Giấy chứng nhận kinh doanh: </strong>0108567789 cấp ngày 18/12/2018</li>
+  						<li><strong>Nơi cấp: </strong>phòng Đăng ký kinh doanh, sở kế hoạch đầu tư Thành phố Hà Nội</li>
+  						<li><strong>Địa chỉ: </strong>Tầng 2, Tòa nhà Trung Yên 1, Khu đô thị Trung Yên, Phường Trung Hòa, Quận Cầu Giấy, Hà Nội</li>
+  						<li><strong>Chịu trách nhiệm nội dung: </strong>(Ông) Phạm Quang Hiển</li>
+  						<li><strong>Thông tin liên hệ: </strong>0246 253 2646</li>
+  						<li><strong>Email: </strong>cskh@ontel.vn</li>
+  					</ul>
+  				</div>
+  			</div>
+  			</div>
+  		</div>
+  	</section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+    <script src="assets/js/app.js"></script>
+  </body>
+</html>
