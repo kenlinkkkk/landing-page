6015a87561df23a0e2fe1d84736262b72bd17e11
NTT <tungnt15ptit@gmail.com>
2021-08-05 23:17:27 +0700

    add view kas

--- kas/assets/js/app.js
+++ kas/assets/js/app.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+	if (navigator.userAgent.indexOf("Android") !== -1) {
+		$('#mobile').css('display', 'block');
+	} else if (navigator.userAgent.indexOf("like Mac") !== -1) {
+		$('#ios').css('display', 'block');
+	} else {
+		$('#pc').css('display', 'block');
+	}
+
 	$(window).on('hashchange', function(e){
 		history.replaceState("", document.title, e.originalEvent.oldURL);
 	});
---
+++ kas/huong-dan.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+<!--    <link href="assets/css/style.css" rel="stylesheet">-->
+
+    <title>Kaspersky | VINVAS</title>
+</head>
+<body>
+<section id="header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="/">
+                <img src="assets/images/klogo1.png" alt="Logo" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" aria-current="page" href="/#">TRANG CHỦ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="/#content-body">GÓI DỊCH VỤ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="/#content-footer">HƯỚNG DẪN ĐĂNG KÝ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active fw-bold" href="/huong-dan-su-dung">HƯỚNG DẪN SỬ DỤNG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="/#footer">LIÊN HỆ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</section>
+<section id="intro-content" style="margin-top: 100px">
+    <div class="container">
+        <div id="mobile" style="display: none;">
+            <h4>Hướng dẫn cài đặt trên điện thoại Android</h4>
+            <h5>Bước 1: Tải ứng dụng Kaspersky Mobile Antivirus trên CH Play - Link tải: <a href="https://play.google.com/store/apps/details?id=com.kms.free&hl=vi">Tại đây</a></h5>
+            <img src="assets/images/mobile-1.jpg" class="img-fluid" alt="Tìm và tải ứng dụng Kaspersky Mobile Antivirus">
+            <h5>Bước 2: Tiến hành cài đặt ứng dụng</h5>
+            <p>Nhấn <span><strong>Mở</strong></span> ứng dụng > Chọn <strong>Tiếp theo</strong> > Chọn <strong>Cho phép.</strong></p>
+            <img src="assets/images/mobile-2.jpg" class="img-fluid" alt="Tiến hành cài đặt ứng dụng sau khi tải xong">
+            <h5>Bước 3: Nhập mã kích hoạt</h5>
+            <p>Chọn <strong>Tôi có một giấy phép</strong> > Chọn <strong>Nhập mã kích hoạt.</strong></p>
+            <img src="assets/images/mobile-3.jpg" class="img-fluid" alt="Nhập mã kích hoạt">
+            <h5>Bước 4: Kết thúc quá trình cài đặt và kích hoạt</h5>
+            <p>Để kết thúc quá trình cài đặt và kích hoạt phần mềm bạn hãy chọn <strong>Tiếp theo.</strong></p>
+            <img src="assets/images/mobile-4.jpg" alt="Sau khi nhập xong mã bạn hãy nhấn Tiếp theo để kết thúc quá trình cài đặt và kích hoạt" class="img-fluid">
+            <p>Sau khi cài đặt thành công phần mềm Kaspersky sẽ hoạt động ẩn để bảo vệ điện thoại của bạn.</p>
+        </div>
+        <div id="pc" style="display: none;">
+            <h4>Hướng dẫn cài đặt và kích hoạt trên máy tính</h4>
+            <h5>Bước 1: Tải về phần mềm Kaspersky - Link tải: <a href="https://www.kaspersky.com/downloads/thank-you/internet-security-free-trial#download">Tại đây</a></h5>
+            <h5>Bước 2: Tiến hành Cài đặt phần mềm Kaspersky</h5>
+            <p>Mở file bạn vừa tải về, sau khi mở file, màn hình sẽ hiển thị giao diện <strong>Welcome to Kaspersky Anti-Virus</strong> bạn hãy nhấn <strong>Continue.</strong></p>
+            <img src="assets/images/pc-1.png" class="img-fluid" alt="Nhấn continue">
+            <p>Tiếp đó bạn chọn <strong>Install.</strong></p>ư
+            <img src="assets/images/pc-2.png" class="img-fluid" alt="Chọn Install để bắt đầu cài đătị phần mềm vào máy tính">
+            <p>Bạn sẽ phải mất khoảng 15 - 30 phút để hoàn tất một quá trình cài đặt tùy vào tốc độ mạng và máy tính của bạn.</p>
+            <img src="assets/images/pc-3.png" class="img-fluid" alt="Quá trình cài đặt sẽ tốn khá nhiều thời gian">
+            <h5>Bước 3: Kết thúc quá trình cài đặt</h5>
+            <p>Khi quá trình cài đặt kết thúc, chọn <strong>Apply</strong> > Hoàn tất để kết thúc quá trình cài đặt.</p>
+            <img src="assets/images/pc-4.png" class="img-fluid" alt="Chọn Apply">
+            <p>Sau khi chọn Áp dụng màn hình sẽ hiển thị thông báo "Ứng dụng được cài đặt thành công" và bạn hãy nhấn <strong>Done.</strong></p>
+            <img src="assets/images/pc-5.png" class="img-fluid" alt="Nhấn Done để kết thúc">
+            <h5>Bước 4: Nhập mã Kích hoạt </h5>
+            <p>Sau khi hoàn tất, chọn License is missing -> Enter activation code để nhập mã kích hoạt.</p>
+            <img src="assets/images/pc-6.png" alt="Nhấn License is missing" class="img-fluid">
+            <p></p>
+            <img src="assets/images/pc-7.png" alt="Nhấn Enter activation code để nhập mã kích hoạt" class="img-fluid">
+            <p></p>
+            <img src="assets/images/pc-8.png" alt="Nhập mã kích hoạt và nhấn Activate để sử dụng" class="img-fluid">
+        </div>
+        <div id="ios" style="display: none;">
+            <h3 class="text-danger">Hiện tại chưa có phiên bản ứng dụng phù hợp hệ điều hành của bạn!</h3>
+        </div>
+    </div>
+</section>
+<section id="footer" class="mt-4">
+    <div class="container-fluid p-4" style="background: radial-gradient(98.91% 64.44% at 1.09% 35.56%, #FFFFFF 0%, #D8FFF9 0.01%, #E6F8DE 100%);">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-4">
+                    <img src="assets/images/klogo2.png" class="img-fluid" alt="Logo">
+                </div>
+                <div class="col-sm-12 col-md-8">
+                    <ul>
+                        <li><strong>CÔNG TY TNHH VINVAS VIỆT NAM</strong></li>
+                        <li><strong>Số giấy phép ICP: </strong>3319/GP-TTĐT cấp ngày 03/07/2019</li>
+                        <li><strong>Giấy chứng nhận ĐKKD số: </strong>0108764836 cấp ngày 31/05/2019 tại Sở KHĐT thành phố Hà Nội</li>
+                        <li><strong>Địa chỉ: </strong>Số 3, ngách 1/8 Đình Thôn, Phường Mỹ Đình 1, Quận Nam Từ Liêm, Hà Nội</li>
+                        <li><strong>Chịu trách nhiệm nội dung:</strong>(Ông) Đặng Quang Hưng</li>
+                        <li><strong>Điện thoại: </strong>0943 417 210</li>
+                        <li><strong>Email: </strong>cskh@vinvas.com</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
